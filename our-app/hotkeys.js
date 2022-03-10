9d86b2ef8606aab13ceaec84cbafbb43bcb61d47
David Don Young Kim <koyykdy@gmail.com>
2022-03-09 23:41:29 -0800

    completed core functionality of extension

--- our-app/hotkeys.js
+++ our-app/hotkeys.js
@@ -1,89 +1,43 @@
 // const copyPasteHandler = require('./copy-paste');
 // import copyPasteHandler from 'copy-paste.js';
 
-/**
-  chrome.tabs.executeScript( {
-    code: "window.getSelection().toString();"
-  }, function(selection) {
-    document.getElementById("output").value = selection[0];
-  });
-
- */
-
-/**
-  async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  }
- */
-
 const copyPasteHandler = {
   handleCopy: (slotNum) => {
     // save to slot num in permanent storage
-    // console.log("inside handleCopy")
     async function getCurentTab() {
       let queryOptions = { active: true, currentWindow: true };
       let [tab] = await chrome.tabs.query(queryOptions);
       return tab;
     }
-    // const currentTab = 
     getCurentTab().then((currentTab) => {
       function getSelection() {
         return window.getSelection().toString();
       }
   
-      // const currentTabId = getTabId(); // does this work???
-  
       chrome.scripting.executeScript( 
         {
-          target: {tabId: currentTab.id}, // <- ERROR: target object has no property 'tabId' ?????
+          target: {tabId: currentTab.id},
           func: getSelection 
         }, 
         (selection) => {
-          // console.log(selection); // <- [{frameId: [number], result: [string]}] // the result is the selected string from window.getSelection().toString();
           const stringSelected = selection[0].result;
-          // console.log(typeof stringSelected);
           const strSlotNum = String(slotNum);
           const setObj = {};
           setObj[strSlotNum] = stringSelected;
           chrome.storage.local.set(setObj, function() {
             chrome.storage.local.get(strSlotNum, (response) => {
-              // console.log(response);
               console.log(`Selection saved in slot ${slotNum}: ` + response[strSlotNum]);
             })
           });
         }
       );
-    }); // currentTab.id <- should be a thing that exists
-    /**
-      chrome.scripting.executeScript({
-      target: {tabId: id, allFrames: true},
-      files: ['content_scripts/cscript.js'],
-      });
-     */
-
-    // chrome.tabs.getCurrent()
-    // .then(function(tab) {
-    //   console.log("inside chrome.tabs.getCurrent");
-    //   console.log(tab);
-    //   if(tab) {
-    //     console.log("inside tabId");
-    //     const SELECTION = window.getSelection().toString(); // <- some string from the window 'hello world'
-    //     console.log(SELECTION);
-    //     chrome.storage.local.set({slotNum: SELECTION}, function() {
-    //       console.log(`Selection saved in slot ${slotNum}: `, SELECTION);
-    //     });
-    //   }
-    // });
+    });
   },
   handlePaste: (slotNum) => {
     // paste from slot num in permanent storage
-    const strSlotNum = String(slotNum); // object = {'strSlotNum' : 'the stuff'}, object['1']
+    const strSlotNum = String(slotNum);
     chrome.storage.local.get(strSlotNum, (response) => { // response = {slotNumber: 'string data'}
-      // do something with the response object
       const strResponse = response[strSlotNum];
-      // console.log(`Accessing value saved in slot ${slotNum}: ` + strResponse); 
       // paste into the current tab where the user is selected, the strResponse
       // `document.querySelector(":focus").textContent += ${strResponse};`
       async function getCurentTab() {
@@ -96,7 +50,6 @@ const copyPasteHandler = {
 
         function pasteText(pasteText) {
           const focusedEl = document.activeElement;
-          // console.log(focusedEl);
           focusedEl.value += pasteText;
         }
     
@@ -107,11 +60,6 @@ const copyPasteHandler = {
             func: pasteText
           }, 
           (returned) => {
-            /**
-              window.addEventListener('focus', function(e) {
-                  if (e.target.id) console.log(e.target.id)
-              }, true);
-             */
             // console log that we successfully pasted what we did
             console.log(`Pasted text from ${slotNum}: ` + strResponse);
           }
@@ -122,11 +70,94 @@ const copyPasteHandler = {
 };
 
 chrome.commands.onCommand.addListener((command) => {
-  if (String(command) === 'multi-copy1') { // then handle it
+  switch (command) {
+    case 'multi-copy1':
+      copyPasteHandler.handleCopy(1);
+      break;
+    case 'multi-paste1':
+      copyPasteHandler.handlePaste(1);
+      break;
+    case 'multi-copy2':
+      copyPasteHandler.handleCopy(2);
+      break;
+    case 'multi-paste2':
+      copyPasteHandler.handlePaste(2);
+      break;
+    // case 'multi-copy3':
+    //   copyPasteHandler.handleCopy(3);
+    //   break;
+    // case 'multi-paste3':
+    //   copyPasteHandler.handlePaste(3);
+    //   break;
+    // case 'multi-copy4':
+    //   copyPasteHandler.handleCopy(4);
+    //   break;
+    // case 'multi-paste4':
+    //   copyPasteHandler.handlePaste(4);
+    //   break;
+    default:
+      console.log('ERROR: Unexpected command encountered!\n');
+  }
+  console.log(`Command: ${command}`); // -> 'multi-copy1' OR 'multi-paste1' OR ...
+});
+
+
+/**
+ * 
+  if (command === 'multi-copy1') { // handle multi-copy1
     copyPasteHandler.handleCopy(1);
   }
-  if (command === 'multi-paste1') {// then handle it
+  if (command === 'multi-paste1') { // handle multi-paste1
     copyPasteHandler.handlePaste(1);
   }
-  console.log(`Command: ${command}`); // -> 'multi-copy1' OR 'multi-paste1'
-});
+  if (command === 'multi-copy2') { // handle multi-copy2
+    copyPasteHandler.handleCopy(2);
+  }
+  if (command === 'multi-paste2') { // handle multi-paste2
+    copyPasteHandler.handlePaste(2);
+  }
+  if (command === 'multi-copy3') { // handle multi-copy3
+    copyPasteHandler.handleCopy(3);
+  }
+  if (command === 'multi-paste3') { // handle multi-paste3
+    copyPasteHandler.handlePaste(3);
+  }
+  if (command === 'multi-copy4') { // handle multi-copy4
+    copyPasteHandler.handleCopy(4);
+  }
+  if (command === 'multi-paste4') { // handle multi-paste4
+    copyPasteHandler.handlePaste(4);
+  }
+ */
+
+/**
+ * ,
+    "multi-copy3": {
+      "suggested_key": {
+        "default": "Alt+Shift+3",
+        "mac": "Alt+Shift+3"
+      },
+      "description": "Copy currently selected text to slot 3"
+    },
+    "multi-paste3": {
+      "suggested_key": {
+        "default": "Alt+3",
+        "mac": "Alt+3"
+      },
+      "description": "Paste text from slot 3"
+    },
+    "multi-copy4": {
+      "suggested_key": {
+        "default": "Alt+Shift+4",
+        "mac": "Alt+Shift+4"
+      },
+      "description": "Copy currently selected text to slot 4"
+    },
+    "multi-paste4": {
+      "suggested_key": {
+        "default": "Alt+4",
+        "mac": "Alt+4"
+      },
+      "description": "Paste text from slot 4"
+    }
+ */
